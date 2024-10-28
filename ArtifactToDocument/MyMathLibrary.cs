4390d9479510a488491b0bf8929a479da1afa728
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-10-28 15:58:27 +0000

    :lipstick: Standarize and fix format

--- ArtifactToDocument/MyMathLibrary.cs
+++ ArtifactToDocument/MyMathLibrary.cs
@@ -52,6 +52,7 @@ public class MyMathLibrary
     private bool IsPrime(int number)
     {
         if (number < 2) return false;
+        
         for (int i = 2; i <= Math.Sqrt(number); i++)
         {
             if (number % i == 0) return false;
